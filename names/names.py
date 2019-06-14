efce9045e0efcf91acd031670f71cea5d1151efc
Jacob McFaul <mcjakedude@gmail.com>
2019-06-14 08:54:05 -0700

    names finished

--- names/names.py
+++ names/names.py
@@ -1,6 +1,35 @@
 import time
-
 start_time = time.time()
+
+class BinarySearchTree:
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+      if value < self.value:
+          if not self.left:
+              self.left = BinarySearchTree(value)
+          else:
+              self.left.insert(value)
+      elif value >= self.value:
+          if self.right is None:
+              self.right = BinarySearchTree(value)
+          else:
+              self.right.insert(value)
+
+  def contains(self, target):
+        if target < self.value:
+            if self.left is None:
+                return False
+            return self.left.contains(target)
+        elif target > self.value:
+            if self.right is None:
+                return False
+            return self.right.contains(target)
+        else:
+            return True
 
 f = open('names_1.txt', 'r')
 names_1 = f.read().split("\n")  # List containing 10000 names
@@ -11,14 +40,24 @@ names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+b = BinarySearchTree('')
+for i in names_1:
+  b.insert(i)
+  
+
+for j in names_2:
+  if b.contains(j):
+    duplicates.append(j)
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
-  
   
